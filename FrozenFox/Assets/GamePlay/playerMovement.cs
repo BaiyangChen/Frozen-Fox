9e26a0800a006e0c067653859f7449095d840981
Baiyang <boyang96@yahoo.ca>
2024-02-22 21:17:01 -0500

    revert

--- FrozenFox/Assets/GamePlay/playerMovement.cs
+++ FrozenFox/Assets/GamePlay/playerMovement.cs
@@ -2,18 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpPower;
+    [SerializeField] private float speed = 3;
     public Vector2 boxSize;
     public float castDistance;
     public LayerMask groundLayer;
-    private bool isFreeze;
-    private float horinzontalInput;
-    private float totalFreezeTime;
 
     public PlayerHealth playerHealth;
     // Start is called before the first frame update
@@ -21,55 +17,30 @@ public class playerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         playerHealth = GetComponent<PlayerHealth>();
-        FindObjectOfType<AudioManager>().Play("BGM");    //Play BGM
         if (playerHealth == null)
         {
             Debug.LogError("PlayerHealth script not found!");
         }
-        speed = 3f;
-        jumpPower = 10f;
-        isFreeze = false;
-        totalFreezeTime = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        horinzontalInput = Input.GetAxis("Horizontal");
-        if (isFreeze)
-        {
-            totalFreezeTime -= Time.deltaTime;
-            jumpPower = 0;
-            speed = 0;
-            if (totalFreezeTime <= 0)
-            {
-                totalFreezeTime = 3;
-                isFreeze = false;
-            }
-        }
-        else if (!isFreeze)
-        {
-            jumpPower = 13f;
-            speed = 3f;
-        }
-    }
-
-    void FixedUpdate()
-    {
+        float horinzontalInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horinzontalInput * speed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGround())
+        if (Input.GetKeyDown(KeyCode.W) && isGround())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            rb.AddForce(new Vector2(rb.velocity.x, 600));
         }
 
-        if (Input.GetKeyDown(KeyCode.Z))
+        if(Input.GetKeyDown(KeyCode.Z))
         {
             playerHealth.TakeDamage(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.X))
+        if(Input.GetKeyDown(KeyCode.X))
         {
             playerHealth.Heal(1);
         }
@@ -105,15 +76,9 @@ public class playerMovement : MonoBehaviour
     {
         if (other.CompareTag("Treat"))
         {
-            //FindObjectOfType<AudioManager>().Play("Collision");    //Play have treat sound
             playerHealth.Heal(1);
             Destroy(other.gameObject);
-        }
-        else if (other.gameObject.name == "SnowBall")
-        {
-            isFreeze = true;
-            Destroy(other.gameObject);
+        
         }
     }
-
 }
